69a1123c06e209409fa9c876b7c1a570136fdda3
Nicolas Martinelli <nicolas.martinelli@gmail.com>
2016-10-01 00:08:05 +0200

    Odoo up to https://github.com/odoo/odoo/commit/ef60b24ce61064d7f7f5674ad2d694c02c442d0f

---
+++ addons/web/tests/test_js.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import odoo.tests
+
+
+class WebSuite(odoo.tests.HttpCase):
+    def test_01_js(self):
+        self.phantom_js('/web/tests?mod=web',"","", login='admin')
