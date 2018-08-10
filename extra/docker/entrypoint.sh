28266e12ad37368c87dd49999c7620a3075778d0
Nicolas Martinelli <nicolas.martinelli@gmail.com>
2018-08-10 22:45:29 +0200

    Prevent access to DB manager in Docker image

--- extra/docker/entrypoint.sh
+++ extra/docker/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 
 service postgresql restart
 sleep 5
-su - koozic -c "/usr/local/koozic/odoo-bin --workers=4 --limit-time-cpu=1800 --limit-time-real=3600 --db_port=54321 --without-demo=all --log-level=warn"
+su - koozic -c "/usr/local/koozic/odoo-bin --workers=4 --limit-time-cpu=1800 --limit-time-real=3600 --db_port=54321 --without-demo=all --no-database-list --log-level=warn"
 
 exit 1
