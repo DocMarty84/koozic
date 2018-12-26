b45c3de307756a2f19937a1d0fe7e72057dd0eae
FL42 <46161216+fl42@users.noreply.github.com>
2018-12-26 13:33:05 +0100

    Use Docker-compose and make container stateless

--- extra/docker/entrypoint.sh
+++ extra/docker/entrypoint.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-set -e
-
-service postgresql restart
+# Wait for Postgres to be up
 sleep 5
-su - koozic -c "/usr/local/koozic/odoo-bin --workers=4 --limit-time-cpu=1800 --limit-time-real=3600 --db_port=54321 --without-demo=all --no-database-list --log-level=warn"
 
-exit 1
+# Give to koozic user the right to write in app volume
+chown -R koozic /home/koozic/.local
+
+# Initialize db (odoo automatically detects if db is already initialized)
+echo "Initializing db..."
+su - koozic -c "/usr/local/koozic/odoo-bin -d koozic \
+			--db_host=db --db_port=5432 \
+			--db_user=koozic --db_password=koozic \
+			-i oomusic,oovideo --without-demo=all --stop-after-init --log-level=warn"
+echo "db initialization done"
+
+# Start koozic
+su - koozic -c "/usr/local/koozic/odoo-bin \
+	--workers=4 --limit-time-cpu=1800 --limit-time-real=3600 \
+	--db_host=db --db_port=5432 \
+	--db_user=koozic --db_password=koozic \
+	--without-demo=all --no-database-list --log-level=warn"
